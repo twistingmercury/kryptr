2b651c1b2b236d8d20b61e7f3c669d8de61cfc65
twistingmercury <jeremy.johnson@mcg.com>
2020-05-20 12:26:57 -0700

    fixed EOF err

--- kryptr.go
+++ kryptr.go
@@ -16,14 +16,14 @@ var (
 	// Date is the date that this version was built.
 	Date string = "who knows?"
 
-	keyflag = flag.BoolP("key-setup", "k", false, "Generate a new security key.")
-	encflag = flag.Bool("encrypt", false, "Encrypt the specified file.")
-	decflag = flag.Bool("decrypt", false, "Decrypt the specified file.")
-	outflag = flag.StringP("out", "o", "", "The name of the output file to create.")
-	//recflag  = flag.StringP("rpwd", "r", "", "Allows the passing in of a password to recover files that were ecrypted with an older security key.")
+	keyflag  = flag.Bool("setup-keys", false, "Generate a new security key.")
+	encflag  = flag.Bool("encrypt", false, "Encrypt the specified file.")
+	decflag  = flag.Bool("decrypt", false, "Decrypt the specified file.")
+	outflag  = flag.StringP("out", "o", "", "The name of the output file to create.")
 	inflag   = flag.StringP("target", "i", "", "The name of the file to be acted upon.")
 	delflag  = flag.BoolP("del-in-file", "d", true, "Deletes the original file that was encrypted.")
 	helpflag = flag.BoolP("help", "h", false, "Displays help information.")
+	//recflag  = flag.StringP("rpwd", "r", "", "Allows the passing in of a password to recover files that were ecrypted with an older security key.")
 )
 
 func main() {
@@ -53,20 +53,23 @@ func setup() {
 	red.Println(warning)
 	fmt.Print("\nUnless you have the recovery password saved somewhere else, all currently encrypted files will be unrecoverable!\nAre you sure you want to do this? (YES | no)> ")
 	var r string
-	_, err := fmt.Scanln(&r)
-	checkErr(err)
+	i, err := fmt.Scanln(&r)
+
+	if err != nil && i > 0 {
+		println(err.Error())
+	}
 
 	if r != "YES" {
 		fmt.Println("Backing away slowly...")
 		os.Exit(0)
 	}
 
-	rpwd, err := kryptomotron.NewKeys()
+	pwd, err := kryptomotron.NewKeys()
 	checkErr(err)
 
 	fmt.Print("\nYour recovery password for the new security key is: ")
-	yellow.Println(rpwd)
-	fmt.Printf("Save this recovery password somewhere safe!  If you loose this password and run kryptr -i again, you will not be able to recover previously encrypted files!\n")
+	yellow.Println(pwd)
+	fmt.Printf("Save this recovery password somewhere safe!  If you loose this password and run kryptr --setup-keys again you will not be able to recover previously encrypted files!\n")
 	os.Exit(0)
 }
 
